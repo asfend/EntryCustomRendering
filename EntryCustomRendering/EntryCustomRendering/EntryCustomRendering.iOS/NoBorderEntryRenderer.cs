7c58cc5f7957026d388dd3039900371d1be06954
DESKTOP-N17J10J\Asfend <asfend@smashcloudmedia.com>
2017-03-04 18:26:00 +0500

    no message

---
+++ EntryCustomRendering/EntryCustomRendering/EntryCustomRendering.iOS/NoBorderEntryRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EntryCustomRendering;
+using EntryCustomRendering.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NoBorderEntry), typeof(NoBorderEntryRenderer))]
+namespace EntryCustomRendering.iOS
+{
+    public class NoBorderEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe from event handlers and cleanup any resources
+            }
+
+            if (e.NewElement != null)
+            {
+                // Configure the native control and subscribe to event handlers
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
